ad6f362148f86d4cd0629b06e0e0a72c62ba7e7b
Raspiduino <raspiduino@github.com>
2022-08-09 08:12:45 +0000

    Fix removing hotplug

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 1 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#1 SMP PREEMPT Sun Nov 7 14:33:14 UTC 2021"
-#define LINUX_COMPILE_BY "giangvinhloc610"
-#define LINUX_COMPILE_HOST "cs-905909293805-default-boost-qwk5t"
-#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
